193a8ea835fa04d97eb46a5e7e9fac7802ad4417
Oscar Valdez <odvesims@gmail.com>
2024-01-26 12:28:26 -0400

    Created component ItemListContainer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './assets/styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   const categories = [
-    { id: 1, name: 'Remeras', path: 'tops' },
-    { id: 2, name: 'Pantalones', path: 'pants' },
-    { id: 3, name: 'Zapatos', path: 'shoes' },
-    { id: 4, name: 'Ropa Interior', path: 'underwear' },
+    { id: 1, name: 'Remeras', path: '/tops' },
+    { id: 2, name: 'Pantalones', path: '/pants' },
+    { id: 3, name: 'Zapatos', path: '/shoes' },
+    { id: 4, name: 'Ropa Interior', path: '/underwear' },
   ];
+
+  const greeting = 'Hola, bienvenido a SweetStore';
+
   return (
     <div className="App">
       <NavBar appName="sweetStore" categories={categories} />
+      <ItemListContainer greeting={greeting} />
     </div>
   );
 }
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,5 @@
+function ItemListContainer({ greeting }) {
+  return <div className="h1 mt-4">{greeting}</div>;
+}
+
+export default ItemListContainer;
